384886fcb57c38c66f63956d4f64c4e95eff6cf9
victor-singha <vicunite2@gmail.com>
2021-06-20 23:04:21 +0530

    encryption and decryption of message

--- js/main.js
+++ js/main.js
@@ -1,20 +1,32 @@
-$(document).ready(function () {
-  $("#home").click(function () {
-    $("#content").load("../pages/home.html");
-  });
-});
-$(document).ready(function () {
-  $("#allorders").click(function () {
-    $("#content").load("../pages/allorders.html");
-  });
-});
-$(document).ready(function () {
-  $("#aboutus").click(function () {
-    $("#content").load("../pages/aboutus.html");
-  });
-});
-$(document).ready(function () {
-  $("#contact").click(function () {
-    $("#content").load("../pages/contact.html");
-  });
-});
+const cipher = (salt) => {
+  const textToChars = (text) => text.split("").map((c) => c.charCodeAt(0));
+  const byteHex = (n) => ("0" + Number(n).toString(16)).substr(-2);
+  const applySaltToChar = (code) =>
+    textToChars(salt).reduce((a, b) => a ^ b, code);
+
+  return (text) =>
+    text.split("").map(textToChars).map(applySaltToChar).map(byteHex).join("");
+};
+
+const decipher = (salt) => {
+  const textToChars = (text) => text.split("").map((c) => c.charCodeAt(0));
+  const applySaltToChar = (code) =>
+    textToChars(salt).reduce((a, b) => a ^ b, code);
+  return (encoded) =>
+    encoded
+      .match(/.{1,2}/g)
+      .map((hex) => parseInt(hex, 16))
+      .map(applySaltToChar)
+      .map((charCode) => String.fromCharCode(charCode))
+      .join("");
+};
+
+var KEY = "MY_SECRET_KEY";
+var plainText = "Hello World";
+const myCipher = cipher(KEY);
+const encrypted = myCipher(plainText); // --> "7c606d287b6d6b7a6d7c287b7c7a61666f"
+console.log(encrypted);
+
+const myDecipher = decipher(KEY);
+const decrypted = myDecipher(encrypted); // --> 'the secret string'
+console.log(decrypted);
