d78d0ec0a77efcb4b71ecbb5ca5333c025dd9626
victor-singha <vicunite2@gmail.com>
2021-06-21 19:27:57 +0530

    get input value

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
 $("#add-item-form").submit(function (e) {
   return false;
 });
+
+var item = "";
+var price = "";
+var customer = "";
+var phone = "";
+
+function getitem() {
+  item = $("#item").val();
+  console.log(item);
+}
+function getprice() {
+  price = $("#price").val();
+  console.log(price);
+}
+function getcustomer() {
+  customer = $("#customer").val();
+  console.log(customer);
+}
+function getphone() {
+  phone = $("#phone").val();
+  console.log(phone);
+}
+function additem() {
+  console.log(item + price);
+}
