3d2f288cbc18cb4f8d9216f453ab7e09fcb3f233
victor-singha <vicunite2@gmail.com>
2021-06-21 15:36:41 +0530

    settings page

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
     $("#home").addClass("text-purple-500");
     $("#allorders").removeClass("text-purple-500");
     $("#aboutus").removeClass("text-purple-500");
-    $("#contact").removeClass("text-purple-500");
+    $("#settings").removeClass("text-purple-500");
   });
 });
 $(document).ready(function () {
@@ -18,7 +18,7 @@ $(document).ready(function () {
     $("#home").removeClass("text-purple-500");
     $("#allorders").addClass("text-purple-500");
     $("#aboutus").removeClass("text-purple-500");
-    $("#contact").removeClass("text-purple-500");
+    $("#settings").removeClass("text-purple-500");
   });
 });
 $(document).ready(function () {
@@ -27,15 +27,15 @@ $(document).ready(function () {
     $("#home").removeClass("text-purple-500");
     $("#allorders").removeClass("text-purple-500");
     $("#aboutus").addClass("text-purple-500");
-    $("#contact").removeClass("text-purple-500");
+    $("#settings").removeClass("text-purple-500");
   });
 });
 $(document).ready(function () {
-  $("#contact").click(function () {
-    $("#content").load("../pages/contact.html");
+  $("#settings").click(function () {
+    $("#content").load("../pages/settings.html");
     $("#home").removeClass("text-purple-500");
     $("#allorders").removeClass("text-purple-500");
     $("#aboutus").removeClass("text-purple-500");
-    $("#contact").addClass("text-purple-500");
+    $("#settings").addClass("text-purple-500");
   });
 });
